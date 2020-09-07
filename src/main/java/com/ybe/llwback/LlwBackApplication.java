65b3fcd9262f849d24374a71127215d94fadd0ad
袁贝尔 <yuanbeier@outlook.com>
2020-09-07 15:03:42 +0800

    初始化后端工程

---
+++ src/main/java/com/ybe/llwback/LlwBackApplication.java
@@ -0,0 +1,13 @@
+package com.ybe.llwback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlwBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlwBackApplication.class, args);
+    }
+
+}
